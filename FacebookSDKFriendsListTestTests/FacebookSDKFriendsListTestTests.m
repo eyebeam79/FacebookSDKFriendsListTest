8cbd261557a87c5644896caed9c51eac5a788bce
Jinho Son <eyebeam79@naver.com>
2014-01-21 22:27:48 +0900

    Initial Commit

---
+++ FacebookSDKFriendsListTestTests/FacebookSDKFriendsListTestTests.m
@@ -0,0 +1,34 @@
+//
+//  FacebookSDKFriendsListTestTests.m
+//  FacebookSDKFriendsListTestTests
+//
+//  Created by Jinho Son on 2014. 1. 21..
+//  Copyright (c) 2014년 STD1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FacebookSDKFriendsListTestTests : XCTestCase
+
+@end
+
+@implementation FacebookSDKFriendsListTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
