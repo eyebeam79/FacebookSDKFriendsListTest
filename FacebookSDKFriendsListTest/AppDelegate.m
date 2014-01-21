370df111b91cc5e997acbb8882f63cdbc2ee0216
Jinho Son <eyebeam79@naver.com>
2014-01-21 22:58:37 +0900

    Facebook SDK를 이용한 친구목록 불러오기 실습을 완료했습니다.

--- FacebookSDKFriendsListTest/AppDelegate.m
+++ FacebookSDKFriendsListTest/AppDelegate.m
@@ -6,9 +6,20 @@
 //  Copyright (c) 2014년 STD1. All rights reserved.
 //
 
+#import <FacebookSDK/FacebookSDK.h>
 #import "AppDelegate.h"
 
 @implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    // Call FBAppCall's handleOpenURL:sourceApplication to handle Facebook app responses
+    BOOL wasHandled = [FBAppCall handleOpenURL:url sourceApplication:sourceApplication];
+    
+    // You can add your app-specific url handling code here if needed
+    
+    return wasHandled;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
--- FacebookSDKFriendsListTest/ViewController.m
+++ FacebookSDKFriendsListTest/ViewController.m
@@ -6,18 +6,55 @@
 //  Copyright (c) 2014년 STD1. All rights reserved.
 //
 
+#import <FacebookSDK/FacebookSDK.h>
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
 @implementation ViewController
+{
+    NSArray *friends;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return friends.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FRIEND_CELL"];
+    NSDictionary<FBGraphUser> *friendList;
+    friendList = friends[indexPath.row];
+    
+    cell.textLabel.text = friendList.name;
+    
+    return cell;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    
+    friends = [[NSArray alloc] init];
+    FBLoginView *loginView = [[FBLoginView alloc] init];
+    // Align the button in the center horizontally
+    loginView.frame = CGRectOffset(loginView.frame, (self.view.center.x - (loginView.frame.size.width / 2)), 5);
+    [self.view addSubview:loginView];
+    
+    FBRequest* friendsRequest = [FBRequest requestForMyFriends];
+    [friendsRequest startWithCompletionHandler: ^(FBRequestConnection *connection,
+                                                  NSDictionary* result,
+                                                  NSError *error) {
+        friends = [result objectForKey:@"data"];
+        NSLog(@"Found: %d friends", (int)friends.count);
+        for (NSDictionary<FBGraphUser>* friend in friends) {
+            NSLog(@"Friend Name: %@, ID: %@", friend.name, friend.id);
+        }
+        [self.table reloadData];
+    }];
 }
 
 - (void)didReceiveMemoryWarning
